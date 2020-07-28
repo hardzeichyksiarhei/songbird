fe6ba1293dede8f28fbc4976eecb6adbf7d59c01
hardz <hardzeichyksiarhei@gmail.com>
2020-07-29 01:14:21 +0300

    feat: add main state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App"></div>
-  );
+import levels from './data/birds';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.levels = levels;
+    this.maxLevel = this.levels.length - 1;
+    this.errorsCount = 0;
+
+    this.audioError = new Audio();
+    this.audioSuccess = new Audio();
+
+    this.state = {
+      currentLevelIndex: -1,
+      currentLevel: null,
+      question: null,
+
+      answers: [],
+      selectedAnswer: null,
+
+      isSuccess: false,
+      score: 0,
+
+      isSounds: true,
+      isGameOver: false
+    };
+  }
+
+  render() {
+    return (
+      <div className="App"></div>
+    );
+  }
 }
 
 export default App;
